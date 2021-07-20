107917348a85fefbf9c7d852e7527737a2cefd43
Fábio <fabio.mendes@profusion.com>
2021-07-20 20:22:27 -0300

    add: modal and its styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface ModalProps {
+  onNewTransactionModal: () => void;
+}
+
+export function Header({ onNewTransactionModal }: ModalProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transferência</button>
+        <button type="button" onClick={onNewTransactionModal}>
+          Nova transferência
+        </button>
       </Content>
     </Container>
   );
